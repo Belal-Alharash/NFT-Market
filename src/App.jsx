f01f610d7de6d199df8888c3e7c124099ea1adf9
yara-sleten <yy5721768@gmail.com>
2025-01-11 14:00:12 +0200

    SocialMediaComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import SocialMediaLink from './components/SocialMediaLink/SocialMediaLink';
 
 export default function App() {
   return (
     <>
-      hello
+     
     </>
   )
 }
