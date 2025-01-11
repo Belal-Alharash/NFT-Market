8bac42431075707dfd4676363a3f9ac8c4b8d920
reemabbas1 <reem24abbas@gmail.com>
2025-01-11 17:49:47 +0300

    Ins.(First Commit) : - initialize  my branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import InfoUlCard from './components/InfoUlCard/InfoUlCard'
 
 export default function App() {
   return (
     <>
       hello
+      <InfoUlCard/>
     </>
   )
 }
