29e162bcce53f962a729083b6baa5b15bef21e58
Eman-Drgham <emandrgham75@gmail.com>
2025-01-13 09:09:07 -0800

    TittelComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './App.css'
+import Title from './components/Title/Title'
+
 
 export default function App() {
   return (
     <>
+      <Title
+        title="Top Creators"
+        paragraph=" Top Rated Creators On The NFT Marketplace"
+      />
+
+      <Title
+        title="Top Creators"
+        paragraph=" Top Rated Creators On The NFT Marketplace"
+        showInput={true}
+        inputPlaceholder="Search your favourite NFTs"
+        iconInput={<i className="fa fa-search"></i>}
+
+      />
+
+      <Title
+        title="Top Creators"
+        paragraph=" Top Rated Creators On The NFT Marketplace"
+        showButton={true}
+        buttonText="Click Me"
+      />
     </>
   )
 }
