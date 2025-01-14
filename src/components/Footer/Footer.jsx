6ad71082f66ab8c181f4c048612506a21766ce3a
Baraa <baraa.alharash82@gmail.com>
2025-01-14 15:20:16 +0300

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import './Footer.css'
+import emailInput from '../EmailInput/EmailInput'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div>
+      <div>
+        <h2></h2>
+        <p></p>
+        <p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </p>
+      </div>
+
+      <div>
+        <h5></h5>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <h5></h5>
+        <p></p>
+        <emailInput/>
+      </div>
+    </div>
+    <p></p>
+    </>
   )
 }
