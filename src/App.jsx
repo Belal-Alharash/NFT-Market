b84961564a727ddb43afe70acb534e7536bad6dd
iyasajeeb <iyasajeeb@gmail.com>
2025-01-14 18:36:55 +0300

    ImageDescription-Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './App.css'
-
 export default function App() {
   return (
     <>
-    </>
+      
+  </>
   )
 }
-
---
+++ src/data/imageDescriptionData.jsx
@@ -0,0 +1,19 @@
+import img1 from '../assets/Images/Artist/Avatar/Avatar(3).png'
+import img2 from '../assets/Images/Artist/Avatar/Avatar(1).png'
+import img3 from '../assets/Images/Artist/Avatar/Avatar(7).png'
+
+
+export const imageDescriptionData = [
+    {
+        userImage: img1 ,
+        userName:'MrFox'
+    },
+    {
+        userImage: img2 ,
+        userName:'Shroomie'
+    },
+    {
+        userImage: img3 ,
+        userName:'BeKind2Robots'
+    }
+]
