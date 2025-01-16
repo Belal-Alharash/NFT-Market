b37a2698a2a6fadadbe633143d5a9f71d07cbf94
Baraa <baraa.alharash82@gmail.com>
2025-01-16 20:52:05 +0300

    FINAL

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,31 +13,33 @@ export default function Footer() {
       <div className='mh-footer-part1'>
         <div className='mh-footer-part1-1'>
           <img className='mh-img-part1-1' src={logo}  alt="logo" />
-          <p className='mh-p-part1-1'>NFT marketplace UI created with Anima for Figma.</p>
-          <div>
-            <span className='mh-span-part1-1'>Join our community</span>
-            <ul className='mh-ul-part1-1'>
-              <li className='mh-li-part1-1'>
-                <a href="https://discord.com/invite/eQxkYTNxSp">
-                  <PiDiscordLogoLight className='mh-a-part1-1' />
-                </a>
-              </li>  
-              <li className='mh-li-part1-1'>
-                <a href="https://www.youtube.com/channel/UCXqr0Ca-b73rU9zcpSBJ5cw">
-                  <CiYoutube className='mh-a-part1-1' />
-                </a>
-              </li>  
-              <li className='mh-li-part1-1'>
-                <a href="https://twitter.com/animaapp?lang=en">
-                  <CiTwitter className='mh-a-part1-1' />
-                </a>
-              </li>  
-              <li className='mh-li-part1-1'>
-                <a href="https://www.instagram.com/animaapp/?hl=en">
-                  <FaInstagram className='mh-a-part1-1' />
-                </a>
-              </li>  
-            </ul>
+          <div className='mh-div-part1-1'>
+            <p className='mh-p-part1-1'>NFT marketplace UI created with Anima for Figma.</p>
+            <div className='mh-div2-part1-1'>
+              <span className='mh-span-part1-1'>Join our community</span>
+              <ul className='mh-ul-part1-1'>
+                <li className='mh-li-part1-1'>
+                  <a href="https://discord.com/invite/eQxkYTNxSp">
+                    <PiDiscordLogoLight className='mh-a-part1-1' />
+                  </a>
+                </li>  
+                <li className='mh-li-part1-1'>
+                  <a href="https://www.youtube.com/channel/UCXqr0Ca-b73rU9zcpSBJ5cw">
+                    <CiYoutube className='mh-a-part1-1' />
+                  </a>
+                </li>  
+                <li className='mh-li-part1-1'>
+                  <a href="https://twitter.com/animaapp?lang=en">
+                    <CiTwitter className='mh-a-part1-1' />
+                  </a>
+                </li>  
+                <li className='mh-li-part1-1'>
+                  <a href="https://www.instagram.com/animaapp/?hl=en">
+                    <FaInstagram className='mh-a-part1-1' />
+                  </a>
+                </li>  
+              </ul>
+            </div>
           </div>
         </div>
         <div className='mh-footer-part1-2'>
