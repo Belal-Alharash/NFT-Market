e9dff894de6d38a8fd480683be71cfd8c2259c73
Ahmad Fwity <ahmad.fwaity@gmail.com>
2025-01-17 17:38:51 +0300

    adding NavBar and react-router-dom and rename images and edit bhbutton

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import NavBarComponent from './components/NavBarComponent/NavBarComponent.jsx';
 
+const root = createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
+root.render(
   <StrictMode>
     <BrowserRouter>
+      <NavBarComponent />
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
