2818a95de19172d1ec0b77a20d46be8b4348ab9c
Baraa <baraa.alharash82@gmail.com>
2025-01-20 01:43:29 +0300

    create First time

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+import MagicMashroom from '../../components/MagicMashroom/MagicMashroom'
 
 
 export default function Home() {
   return (
     <>
     <Hero />
+    <MagicMashroom/>
     </>
   )
 }
