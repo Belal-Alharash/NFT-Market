2d6c05bb78d05673209a5be1cd9c12e58542820c
hanannoman <hanannoman392003123@gmail.com>
2025-01-22 23:11:10 +0300

    artistHeader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+import ArtistPageHero from '../../components/ArtistPageHero/ArtistPageHero'
 
 
 export default function Home() {
   return (
     <>
     <Hero />
+ 
     </>
   )
 }
--- src/pages/MarketPlace/MarketPlace.jsx
+++ src/pages/MarketPlace/MarketPlace.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './MarketPlace.css'
+import ArtistPageHero from '../../components/ArtistPageHero/ArtistPageHero'
 
 export default function MarketPlace() {
   return (
     <>
-        MarketPlace
+       
+        <ArtistPageHero/>
     </>
   )
 }
