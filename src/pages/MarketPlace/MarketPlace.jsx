c1207b88b32a468c25d8d4781dd60f6efb1b03b1
hanannoman <hanannoman392003123@gmail.com>
2025-01-22 23:58:47 +0300

    artistPageHeader

--- src/pages/MarketPlace/MarketPlace.jsx
+++ src/pages/MarketPlace/MarketPlace.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import './MarketPlace.css'
-import ArtistPageHero from '../../components/ArtistPageHero/ArtistPageHero'
-
 export default function MarketPlace() {
   return (
     <>
-       
-        <ArtistPageHero/>
+       <div>MarketPlace</div>
     </>
   )
 }
