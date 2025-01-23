89fe03c87c89c3b7550e9868f0f52fc75c810458
iyasajeeb <iyasajeeb@gmail.com>
2025-01-23 14:37:56 +0300

    Image Description Component

--- src/components/ImageDescription/ImageDescription.jsx
+++ src/components/ImageDescription/ImageDescription.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react'
 import './ImageDescription.css'
 
-export default function ImageDescription() {
+export default function ImageDescription({ userImage, userName, bordered, font }) {
+
   return (
-    <div>ImageDescription</div>
+    <div className={bordered ? "iA-image-description-BorderedFill" : "iA-image-description"}>
+      <img src={userImage} alt='userImage' />
+      <p className={font ? "iA-image-description-WorkSans-font" : "iA-image-description-SpaceMono-Font"}>{userName}</p>
+    </div>
   )
 }
