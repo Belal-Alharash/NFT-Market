eab0eeea43848a045a706613a02bb00b2f7b760b
Baraa <baraa.alharash82@gmail.com>
2025-01-26 20:37:00 +0300

    all data main card

---
+++ src/data/HomeMainCard.js
@@ -0,0 +1,28 @@
+export const HomeMainCard =[
+    {
+        mainImg:"../../public/Images/DiscoverMoreNFTs/DistantGalaxy.jpg",
+        title:"Distant Galaxy",
+        subImg:"../../public/Images/DiscoverMoreNFTs/MoonDancer.png",
+        dsecImg:"MoonDancer",
+        price:"1.63 ETH",
+        highestBid:"0.33 wETH"
+    }
+    ,
+    {
+        mainImg:"../../public/Images/DiscoverMoreNFTs/LifeOnEdena.jpg",
+        title:"Life On Edena",
+        subImg:"../..//public/Images/DiscoverMoreNFTs/NebulaKid.png",
+        dsecImg:"NebulaKid",
+        price:"1.63 ETH",
+        highestBid:"0.33 wETH"
+    }
+    ,
+    {
+        mainImg:"../../public/Images/DiscoverMoreNFTs/AstroFiction.jpg",
+        title:"AstroFiction",
+        subImg:"../../public/Images/DiscoverMoreNFTs/Spaceone.png",
+        dsecImg:"Spaceone",
+        price:"1.63 ETH",
+        highestBid:"0.33 wETH"
+    }
+]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+import MagicMashroom from '../../components/MagicMashroom/MagicMashroom'
 export default function Home() {
   return (
     <>
     <Hero />
-        </>
-  )``
+    <MagicMashroom/>
+    </>
+  )
 }
