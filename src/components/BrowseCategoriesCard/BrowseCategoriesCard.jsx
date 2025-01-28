903c32f67e9a0cec00271bbfc17ef1c5db7e71c9
hanannoman <hanannoman392003123@gmail.com>
2025-01-28 22:39:55 +0300

    BrowseSection&ArtistHeader

--- src/components/BrowseCategoriesCard/BrowseCategoriesCard.jsx
+++ src/components/BrowseCategoriesCard/BrowseCategoriesCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './BrowseCategoriesCard.css'
 
+
 export default function BrowseCategoriesCard({photo,title}) {
   return (
-    <div className='hn-cardBrows'>
-<img src={photo} alt= 'image' className='hn-image' />
+      
+   <div className='hn-cardBrows'>
+   
+ <img src={photo} alt= 'image' className='hn-image' />
   <h5 className='hn-title'>{title}</h5>
-    </div>
+ 
+
+   </div>
+  
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+
 export default function Home() {
-  return (
+  return(
     <>
     <Hero />
-        </>
-  )``
+ 
+    </>  
+  )
+  
+  
 }
