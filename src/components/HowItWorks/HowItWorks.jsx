db48cffcbd25cc0b9257f2ed900ad2f8d6c8c64a
yara-sleten <yy5721768@gmail.com>
2025-01-28 21:26:16 +0200

    nft-page-hero

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+
 import './HowItWorks.css'
+import Title from '../Title/Title'
+import HowItWorksCard from '../HowItWorksCard/HowItWorksCard'
+import { HowItWorkCardData } from '../../data/dataHowItWorksCard'
 
 export default function HowItWorks() {
   return (
-    <div>HowItWorks</div>
+    <section className='my-section-how-it-work-ys'>
+      <div className='sec-How-it-work-ys'>
+      <div className='title-how-it-work-ys' >
+         <Title  magic_h3={true} title={'How It Works'} paragraph={'Find Out How To Get Started'}/>
+      </div>
+      <div className='div-cards-how-it-work-ys'>
+        { HowItWorkCardData.map(( y , index) => {
+          return(
+            <HowItWorksCard key={index} title={y.title} img={y.img} text={y.text} />
+          )
+        }
+        )
+        }
+      </div>
+     </div>
+    </section>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,12 @@ import './Home.css'
 import Hero from '../../components/Hero/Hero'
 import MagicMashroom from '../../components/MagicMashroom/MagicMashroom'
 
-
 export default function Home() {
   return (
     <>
     <Hero />
     <MagicMashroom/>
+
     </>
   )
 }
