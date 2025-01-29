4f198f6234953f7ece234b5a0ab872e0fd16890f
yara-sleten <yy5721768@gmail.com>
2025-01-29 21:07:21 +0200

    trinding-card

--- src/components/CreatorsRankingCard/CreatorsRankingCard.jsx
+++ src/components/CreatorsRankingCard/CreatorsRankingCard.jsx
@@ -1,7 +1,5 @@
 import "./CreatorsRankingCard.css";
-
-
-export default function CreatorsRankingCard({ page, id, img, title, percent, sales }) {
+export default function CreatorsRankingCard({ page, id, img, title, percent,description, sales }) {
   const isHomePage = page === "home";
 
   return (
@@ -10,19 +8,26 @@ export default function CreatorsRankingCard({ page, id, img, title, percent, sal
         <div className={isHomePage ? "af-creators-card-id-pop" : "af-ranking-card-id-pop"}>
           <p className={isHomePage ? "af-creators-card-id" : "af-ranking-card-id"}>{id}</p>
         </div>
+        <div className={isHomePage ? "ys-desc-creators" : "ys-desc-ranking"}>
         <img
           className={isHomePage ? "af-creators-card-img" : "af-ranking-card-img"}
           src={img}
           alt={`Image of artist: ${title}`}
         />
+        <h5 className={isHomePage ? "d-none" :"af-card-ranking-name"}>{title}</h5>
+        </div>
       </div>
       <div className={isHomePage ? "af-creators-card-text" : "af-ranking-card-text"}>
-        <h5 className="af-card-artist-name">{title}</h5>
-        <p className={isHomePage ? "d-none" : "af-ranking-card- percent"}>
+        <h5 className={isHomePage ? "af-card-artist-name" :"d-none"}>{title}</h5>
+        <p className={isHomePage ? "d-none" : "af-ranking-card-percent"}>
           {percent}
         </p>
-        <div className={isHomePage ? "af-creators-card-sales" : "af-ranking-card-sales"}>
-          <p>Total Sales:</p> <span>{sales}</span>
+        <p className={isHomePage ? "d-none" : "af-ranking-card-description"}>
+          {description}
+        </p>
+        <p className={isHomePage ? "d-none" : "af-ranking-card-sales"}>{sales} </p>
+        <div className={isHomePage ? "af-creators-card-sales" : "d-none"}>
+          <p>Total Sales:</p> <span >{sales}</span>
         </div>
       </div>
       </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
 import MagicMashroom from '../../components/MagicMashroom/MagicMashroom'
-import EmailSection from '../../components/EmailSection/EmailSection'
-import NftMainCard from '../../components/NftMainCard/NftMainCard'
-
+import CreatorsRankingCard from '../../components/CreatorsRankingCard/CreatorsRankingCard'
+import { dataRankingTopCreators } from '../../data/dataRankingTopCreators'
 export default function Home() {
   return (
     <>
     <Hero />
     <MagicMashroom/>
+
+          <CreatorsRankingCard id={'1'} img={'./public/Images/ArtistAvatar/Keepitreal.png'}
+           title={'Jaydon Ekstrom'} percent={'+1.41%'} description={'602'}
+          sales={'12.4 ETH'} page={'ranking'} />
 
     </>
   )
